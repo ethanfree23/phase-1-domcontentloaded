cf1cfdcb84411ebfb56ddabc9084693c5c296287
Ethan Freeman <ethanfree23@gmail.com>
2021-12-02 12:42:20 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+
+// let text = document.getElementById('p')
+// text.innerText = 'This is really cool!'
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("The DOM has loaded");
+  });
+
+  let text = document.getElementById('p')
+  text.innerText = 'This is really cool!'
+
+
+// document.addEventListener("DOMContentLoaded", function(e) { console.log("The DOM has loaded") } );
+
+// let text = document.querySelector('p')
+//   text.innerHTML = 'This is really cool!'
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log("This is really cool!")
+// })
+
+// let text = document.getElementById('p');
+// text.innerHTML = 'JavaScript is so cool. It lets me add text to my page programmatically'
+
+// let text = document.querySelector('p')
+// text.innerHTML = 'JavaScript is so cool. It lets me add text to my page programmatically.'
