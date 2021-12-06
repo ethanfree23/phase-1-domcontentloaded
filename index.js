b799e3b81aba3791cdd450d5d4c1e887a228c685
Ethan Freeman <ethanfree23@gmail.com>
2021-12-05 18:57:35 -0600

    commit tree 1e83e3d43691f8ae8dbd3d0763a7e756a5051ece

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 // Your code goes here
 
 
+
+
+let text = document.querySelector('#text');
+document.addEventListener("DOMContentLoaded", () => {
+  text.textContent = "This is really cool!"
+})
+
+
+
+// let text = document.getElementById('text'); // this line sets a variable equal to the ID in the HTML, which is "text"
+  // document.addEventListener("DOMContentLoaded", (event) => { // this line calls the doc, adds event listener, says while content is being loaded, the event to trigger...
+  // text.textContent = `This is really cool!` // is, changing the textContent of the ID text's corresponding <p></p> to a new string
+  // })
+
+
 // let text = document.getElementById('p')
 // text.innerText = 'This is really cool!'
 
-
-  let text = document.getElementById('text');
-  document.addEventListener("DOMContentLoaded", (event) => {
-  text.textContent = `This is really cool!`
-  })
 
   // below is Elani's code
   // const textToChange = document.getElementById('text');
